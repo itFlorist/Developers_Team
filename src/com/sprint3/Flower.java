6d223e01b50ec0d9ef4ca7b8a541594a47318c1f
oliverGarniu <oliver.begur@gmail.com>
2022-01-24 17:08:57 +0100

    commit 1 Flower

--- src/com/sprint3/Flower.java
+++ src/com/sprint3/Flower.java
@@ -2,4 +2,55 @@ package com.sprint3;
 
 public class Flower {
 
+	private int flowerId;
+	private String flowerName;
+	private String flowerColour;
+	private float flowerPrice;
+
+	public Flower(int flowerId, String flowerName, String flowerColour, float flowerPrice) {
+		super();
+		this.flowerId = flowerId;
+		this.flowerName = flowerName;
+		this.flowerColour = flowerColour;
+		this.flowerPrice = flowerPrice;
+	}
+
+	public int getFlowerId() {
+		return flowerId;
+	}
+
+	public void setFlowerId(int flowerId) {
+		this.flowerId = flowerId;
+	}
+
+	public String getFlowerName() {
+		return flowerName;
+	}
+
+	public void setFlowerName(String flowerName) {
+		this.flowerName = flowerName;
+	}
+
+	public String getFlowerColour() {
+		return flowerColour;
+	}
+
+	public void setFlowerColour(String flowerColour) {
+		this.flowerColour = flowerColour;
+	}
+
+	public float getFlowerPrice() {
+		return flowerPrice;
+	}
+
+	public void setFlowerPrice(float flowerPrice) {
+		this.flowerPrice = flowerPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "Flower [flowerId=" + flowerId + ", flowerName=" + flowerName + ", flowerColour=" + flowerColour
+				+ ", flowerPrice=" + flowerPrice + "]";
+	}
+
 }
