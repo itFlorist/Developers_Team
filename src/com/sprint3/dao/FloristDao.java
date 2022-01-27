75e6ddf40bda572f4d06e97c60218c46f717d76b
ValmarUr <monesvol@gmail.com>
2022-01-27 18:05:42 +0100

    Update and rename FloristDao.java to FloristManagerDao.java
    .

--- src/com/sprint3/dao/FloristDao.java
+++
@@ -1,9 +0,0 @@
-package com.sprint3.dao;
-
-import java.util.List;
-import com.sprint3.dto.Product;
-
-public interface FloristDao {
-
-
-}
---
+++ src/com/sprint3/dao/FloristManagerDao.java
@@ -0,0 +1,9 @@
+package com.sprint3.dao;
+
+import java.util.List;
+import com.sprint3.dto.Florist;
+
+public interface FloristManagerDao {
+
+
+}
