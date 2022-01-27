28b7943cef71ef2c0727e4e293443c62e8060e3a
ValmarUr <monesvol@gmail.com>
2022-01-27 18:04:05 +0100

    Rename FloristController.java to FlorisController.java
    Product -> Florist

--- src/com/sprint3/controller/FlorisController.java
+++ src/com/sprint3/controller/FlorisController.java
@@ -2,23 +2,23 @@ package com.sprint3.controller;
 
 import java.util.List;
 
-import com.sprint3.dao.ProductDao;
+import com.sprint3.dao.FloristDao;
 
 import com.sprint3.dto.Tree;
 
-import com.sprint3.dto.Product;
+import com.sprint3.dto.Florist;
 
-import com.sprint3.gui.ProductView;
+import com.sprint3.gui.FloristView;
 
-public class ProductController {
+public class FloristController {
 
-	private static ProductView productView;
-	private ProductDao productDao;
+	private static FloristView floristView;
+	private FloristDao floristDao;
 	private static List<Product> stock;
 
-	public ProductController(ProductView productView, ProductDao productDao) {
-		this.productView = productView;
-		this.productDao = productDao;
+	public FloristController(FloristView floristView, FloristDao floristDao) {
+		this.floristView = floristView;
+		this.floristDao = floristDao;
 	}
 
 	public static void runProduct() {
@@ -65,44 +65,44 @@ public class ProductController {
 		}
 	}
 
-	private static void add(List<Product> stock) {
+	private static void add() {
 
 	}
 
-	private static void delete(List<Product> stock) {
+	private static void delete() {
 
 	}
 
-	private static void showFloristStock(List<Product> stock) {
+	private static void showFloristStock() {
 		
 		//Oliver
 
 	}
 
-	private static void showFloristValue(List<Product> stock) {
+	private static void showFloristValue() {
 
 	}
 
-	private static void showCurrentPurchaseReceipt(List<Product> stock) {
+	private static void showCurrentPurchaseReceipt() {
 
 	}
 
-	private static void showOldPurchaseReceipts(List<Product> stock) {
+	private static void showOldPurchaseReceipts() {
 		
 		//Oliver
 
 	}
 
-	private static void totalIncome(List<Product> stock) {
+	private static void totalIncome() {
 
 	}
 
 	private static int getMenuProduct() {
-		return productView.menuProduct();
+		return floristView.menuProduct();
 	}
 
 	private static void unknownCommand() {
-		productView.displayUnknownCommandBanner();
+		floristView.displayUnknownCommandBanner();
 	}
 
 	private void addProduct() {
@@ -133,18 +133,18 @@ public class ProductController {
 	}
 
 	private void addTree() {
-		productView.displayCreateTreeBanner();
-		Tree newTree = productView.getNewTreeInfo();
-		productDao.addTree(newTree.getId(), newTree);
-		productView.displayCreateSuccessBanner();
+		floristView.displayCreateTreeBanner();
+		Tree newTree = floristView.getNewTreeInfo();
+		floristDao.addTree(newTree.getId(), newTree);
+		floristView.displayCreateSuccessBanner();
 
 	}
 
 	private int getAddMenuSelection() {
-		return productView.printAddMenuAndGetSelection();
+		return floristView.printAddMenuAndGetSelection();
 	}
 
 	private void unknownCommand() {
-		productView.displayUnknownCommandBanner();
+		floristView.displayUnknownCommandBanner();
 	}
 }
