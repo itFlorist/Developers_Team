c8de04d42a0599b05640a7e578ca6207ef06e917
Alberto <alberto.sagi@gmail.com>
2022-02-02 16:58:36 +0100

    4. Show Florist value

--- src/com/sprint3/gui/FloristView.java
+++ src/com/sprint3/gui/FloristView.java
@@ -1,10 +1,6 @@
 package com.sprint3.gui;
 
-
-import java.util.List;
-
 import com.sprint3.dto.Decoration;
-import com.sprint3.dto.Florist;
 import com.sprint3.dto.Flower;
 import com.sprint3.dto.Product;
 import com.sprint3.dto.Tree;
@@ -164,6 +160,19 @@ public class FloristView {
 
 	public void unknownCommand() {
 		io.print("Unknown command!");
+
+	}
+
+	/**
+	 * Method that shows on console the value recieved by parameter.
+	 * 
+	 * @param value
+	 */
+	public void displayStockValue(float value) {
+		io.print("\n*******STOCK VALUE******");
+		io.print("--------------------------");
+		io.print("       " + value + "€     ");
+		io.print("--------------------------");
 
 	}
 
